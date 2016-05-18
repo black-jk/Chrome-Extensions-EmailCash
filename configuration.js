c22c7ba358697ae790e69f99e22a08c2d247b7a6
blackjk <blackjk0@gmail.com>
2016-05-18 22:51:19 +0800

    [feature] support chrome options

--- configuration.js
+++ configuration.js
@@ -11,11 +11,20 @@
     
     // --------------------------------------------------
     
-    debug: /**/ true /*/ false /**/,
+    debug: /*/ true /*/ false /**/,
     
     redirectDelay: /*/ 3000 /*/ 0 /**/
-    
   }
+  
+  // [chrome.storage]
+  chrome.storage.sync.get({
+    debug: false,
+    redirect_delay: 0
+  }, function(items) {
+    Config.debug = items.debug;
+    Config.redirectDelay = items.redirect_delay;
+    console.debug("[Config]", Config);
+  });
   
   
   
