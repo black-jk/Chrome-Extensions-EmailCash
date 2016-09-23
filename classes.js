777fa5965b595ed41ef0dd5790ff0c595c7a8f11
blackjk <blackjk0@gmail.com>
2016-09-23 15:06:04 +0800

    [refactor] move check login to function

--- classes.js
+++ classes.js
@@ -83,23 +83,11 @@
     
     // ----------------------------------------------------------------------------------------------------
     
-    this.autoLogin = true;
-    
     this.run = function() {
       this.start();
       
       // check login
-      $login_link = $("a:contains('會員登入')");
-      if ($login_link != null && $login_link.length > 0) {
-        Logger.log("Loggin link found: '" + $login_link.text().trim() + "'");
-        
-        if (this.autoLogin) {
-          var href = $login_link.attr("href");
-          this.goto(href, Config.redirectDelay);
-          Logger.log("Goto '" + href + "'");
-        } else {
-          Logger.log("Waiting for login!");
-        }
+      if (!this.checkLogin()) {
         return;
       }
       
@@ -113,9 +101,34 @@
       Logger.debug("START");
     };
     
+    // --------------------------------------------------
+    
+    this.autoLogin = true;
+    
+    this.checkLogin = function() {
+      $login_link = $("a:contains('會員登入')");
+      if ($login_link != null && $login_link.length > 0) {
+        Logger.log("Loggin link found: '" + $login_link.text().trim() + "'");
+        
+        if (this.autoLogin) {
+          var href = $login_link.attr("href");
+          this.goto(href, Config.redirectDelay);
+          Logger.log("Goto '" + href + "'");
+        } else {
+          Logger.log("Waiting for login!");
+        }
+        return false;
+      }
+      return true;
+    };
+    
+    // --------------------------------------------------
+    
     this.operation = function() {
       throw new Error("[" + this.title + "] Override this function first!");
     };
+    
+    // --------------------------------------------------
     
     this.done = function() {
       Logger.debug("DONE");
