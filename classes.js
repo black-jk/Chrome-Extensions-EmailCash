8e4309e102c98aa5a4bf1e40012637cd87fbebb3
blackjk <blackjk0@gmail.com>
2016-10-19 23:55:06 +0800

    [refactor] auto sent numbers

--- classes.js
+++ classes.js
@@ -308,8 +308,77 @@
       Logger.debug("Click AutoNumber.");
       $(".AutoNumber").click();
       
+      /*/
       Logger.debug("Click SentFun.");
       $(".SentFun").click();
+      /*/
+      
+      // act:0
+      // nick:w214nt8f4f2o
+      // numStr:|89|734|590|40|833|298|131|
+      
+      // copy from http://www.emailcash.com.tw/4G/js/games/DailyGamesJS.js
+      var BetNumStr="|";
+      var errmark=0;
+      var n = $(".GuessNoList input").length;
+      
+      for (var i=0; i<n; i++) {
+          var BetNum = $("#ctl00_mainPlaceHolder_repeaterGuessInput_ctl0" + i + "_txtBetNum").val().trim();
+          if (BetNum.length>0){
+              if (!isNaN(BetNum)) {
+                  if (BetNum > 0 && BetNum < 1000) {
+                      $("#ctl00_mainPlaceHolder_repeaterGuessInput_ctl0" + i + "_txtBetNum").removeClass("Markfoucs");
+                      BetNumStr = BetNumStr + BetNum + "|";
+                  }
+                  else {
+                      $("#ctl00_mainPlaceHolder_repeaterGuessInput_ctl0" + i + "_txtBetNum").addClass("Markfoucs");
+                      errmark = 2;
+                  }
+              }else{
+                  $("#ctl00_mainPlaceHolder_repeaterGuessInput_ctl0" + i + "_txtBetNum").addClass("Markfoucs");
+                  errmark=1;
+              }
+          }else{
+              $("#ctl00_mainPlaceHolder_repeaterGuessInput_ctl0" + i + "_txtBetNum").addClass("Markfoucs");
+              errmark=1;
+          }
+      }
+      
+      //設定存入DB的號碼
+      $("#ctl00_mainPlaceHolder_hidBetNumStr").val(BetNumStr);
+      
+      var nick = $("#ctl00_mainPlaceHolder_hidNick").val();
+      var numStr = $("#ctl00_mainPlaceHolder_hidBetNumStr").val();
+      var datastr = "act=0&nick=" + nick + "&numStr=" + numStr;
+      
+      $.ajax({
+          type: "POST",
+          url: "DailyGamesAjax.aspx",
+          data: datastr,
+          success: function (res) {
+              if (res == "1") {
+                  
+                  $("#ctl00_mainPlaceHolder_hidBetNumStr").value = "";
+                  window.location.reload();
+              }
+              else {
+                  alert("發生錯誤，請重新再試。");
+                  $("#loading").hide();
+                  $(".loadingimg").hide();
+              }
+              
+          },
+          beforeSend: function () {
+              $("#loading").show();
+              $(".loadingimg").show();
+          },
+          error: function (xhr, ajaxOptions, thrownError) {
+              alert("發生錯誤，請重新再試。");
+              $("#loading").hide();
+              $(".loadingimg").hide();
+          }
+      });
+      /**/
       
       // $("td[class=GuessNoList]").find("input").each(function(index, object) { object.value = index; });
     };
