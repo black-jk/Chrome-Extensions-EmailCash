e40b7beb67dc64c7639b638508c9bc8fe7dbdb21
blackjk <blackjk0@gmail.com>
2018-05-10 00:57:55 +0800

    [next-release] [AccountOperator] set table width

--- src/ec/operators/AccountOperator.js
+++ src/ec/operators/AccountOperator.js
@@ -15,9 +15,12 @@ export class AccountOperator extends Operator {
   operation() {
     ///<a href="account.asp?go=points&amp;u=w214nt8f4f2o&amp;c=B0EE1F5580D0F7A&amp;st=last"><b>&lt;&lt;</b> 上月明細</a>
 
+    $(`table[class="tbList"]`).width(350);
+
     Logger.log('scroll to end');
     let scrollTop: Number = Math.max(0, $("td a:contains('上月明細')").offset().top - 100);
     $("body").scrollTop(scrollTop);
+    Logger.log(`body.scrollTop = ${scrollTop}`);
 
     /*
     // ok
