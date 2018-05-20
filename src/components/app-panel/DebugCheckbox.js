89996a473c4820a08a2f7dd8505c517b9ad11a0a
blackjk <blackjk0@gmail.com>
2018-05-20 23:22:34 +0800

    [next-release] [refactor] update views of AppPanel

--- src/components/app-panel/DebugCheckbox.js
+++ src/components/app-panel/DebugCheckbox.js
@@ -2,32 +2,29 @@
 // [DebugCheckbox]
 // ====================================================================================================
 import React, { Component } from 'react';
-import { Label, Input } from 'reactstrap';
+import { Button } from 'reactstrap';
 import { AppConfig } from '../../global';
 import { EmailCacheConfig } from '../../lib/ChromeStorage';
 
 export class DebugCheckbox extends Component {
 
   render() {
+    let backgroundColor: String = (EmailCacheConfig.debug) ? "#28a745" : "";
+
     return (
       <span>
-        <Label check>
-          <Input id="debug" type="checkbox"
-            checked={EmailCacheConfig.debug ? true : false}
-            onChange={this._handleChange} />{`Debug`}
-        </Label>
+        <Button size="sm" style={{ width: 80, backgroundColor: backgroundColor }}
+          onClick={this._handleClick}>{`Debug: ${EmailCacheConfig.debug ? "on" : "off"}`}</Button>
       </span>
     );
   }
 
   // ----------------------------------------------------------------------------------------------------
 
-  _handleChange = (event: Event) => {
-    EmailCacheConfig.debug = event.currentTarget.checked;
-    // console.log(`[CHANGE] [EmailCacheConfig.debug]`, EmailCacheConfig.debug);
+  _handleClick = (event) => {
+    EmailCacheConfig.debug = !EmailCacheConfig.debug;
+    // console.log(`[CHANGE] []`, EmailCacheConfig.debug);
     this.forceUpdate();
-
-    EmailCacheConfig.save([`debug`]);
   }
 
 };
